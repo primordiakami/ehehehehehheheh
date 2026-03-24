eed29f12739e968a4176b7a419d04d93c4aa8823
primordiakami <primordiakami@gmail.com>
2026-03-24 10:59:29 +0000

    Fix build.gradle.kts

--- AIAutoClicker/settings.gradle.kts
+++ AIAutoClicker/settings.gradle.kts
@@ -1,21 +1,11 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://chaquo.com/maven")
-        gradlePluginPortal()
-    }
-}
-
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://chaquo.com/maven")
-        maven("https://jitpack.io")
-    }
+        repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+            repositories {
+                        google()
+                                mavenCentral()
+                                        maven("https://chaquo.com/maven")
+                                                maven("https://jitpack.io")
+            }
 }
-
-rootProject.name = "AIAutoClicker"
-include(":app")
+            }
+}
